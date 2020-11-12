05c1c0ef1b21909c18b586e0011107568cd8fd53
Dorgival Rocha <dojak220@ufrn.edu.br>
2020-11-11 23:29:09 -0300

    mais duas funcionalidades

--- mainwindow.h
+++ mainwindow.h
@@ -33,19 +33,22 @@ private slots:
 
     void on_btn_continuar_clicked();
 
-    void on_btn_m_prio_clicked();
-
-    void on_btn_l_prio_clicked();
-
     void refreshTable();
 
     void fillTable();
 
     void on_ledit_filter_textChanged(const QString &arg1);
 
+    void on_btn_prio_clicked();
+
+    void on_btn_cpu_clicked();
+
 private:
     Ui::MainWindow *ui;
     QTimer *timer;
+    QString pid = 0;
+    QString ni = 0;
+    QString cpu = 0;
     QTableWidgetItem *pid_selected;
     QString filter_text = "";
     char bash_command[105] = "";
